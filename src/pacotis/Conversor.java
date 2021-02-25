62b1d3b94a36459b3fb5d3105f8ae6bd8feb2916
usaze <https://github.com/ETEC-Francisco-Morato-211/Calculadora_Simples>
2021-02-24 22:24:50 -0300

    Commit do Uelder dia 24 de fevereiro 2021

--- src/pacotis/Conversor.java
+++ src/pacotis/Conversor.java
@@ -6,6 +6,7 @@
 package pacotis;
 
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -14,62 +15,63 @@ import java.util.Scanner;
 public class Conversor {
     
     
-    public static void main(String[] args) {
+     public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        double vFah, vCel, celsius, fahrenheit;
+        int  conversor;
+
+
+        JOptionPane.showMessageDialog(null,"\n\t\t\t!========== CONVERSOR DE TEMPERATURAS SHOW DE BOLA ==========!\n"
+        + "__________________________________________\n");
+        conversor = Integer.parseInt(JOptionPane.showInputDialog(null,"\n\t\t\\_____MENU____/\n" + "OPÇÕES DE CONVERSÃO DE TEMPERATURAS \n" +
+                "\n [1] - ConVerter Celsius para Fahrenheit" + "\n [2] - Converter Fahrenheit para Celsius" + "\n [3] - Sair do Programa"));
+
+        switch(conversor){
+
+            case 1:
+
+                vCel = Double.parseDouble(JOptionPane.showInputDialog(null,"Por favor informe a temperatura em ºC: "));
+
+                JOptionPane.showMessageDialog(null,"\n EXECUTANDO CONVERSÃO POR FAVOR AGUARDE");
+                fahrenheit = (9 * vCel / 5) + 32;
+
+                JOptionPane.showMessageDialog(null,"SUA CONVERSÃO ESTA PRONTA\n");
+                JOptionPane.showMessageDialog(null,"A TEMPERATURA EM FAHRENHEIT É %.2f\n" + fahrenheit);
+                break;
+
+            case 2:
+
+                vFah = Double.parseDouble(JOptionPane.showInputDialog("Por favor informe a temperatura em ºF: "));
+
+                JOptionPane.showMessageDialog(null,"\n EXECUTANDO CONVERSÃO POR FAVOR AGUARDE");
+                celsius = (vFah-32) * 5 / 9;
+
+                JOptionPane.showMessageDialog(null,"SUA CONVERSÃO ESTA PRONTA\n");
+                JOptionPane.showMessageDialog(null,"\n A TEMPERATURA EM CELSIUS É %.2f\n");
+                break;
+
+            case 3:
+
+                JOptionPane.showMessageDialog(null,"GOOD BYE");
+                break;
+
+            default:
+
+                JOptionPane.showMessageDialog(null,"OPÇÃO INVÁLIDA!");
+
+
+
+        }
 
 
 
 
-        //Aqui criamos um objeto para receber os dados pelo usuário.
-        Scanner entrada = new Scanner (System.in);
-
-        //Área para declaração de variáveis.
-        double vCel, celsius, vFah,fahrenheit;
-        int conversor;
-
-        do {
-            System.out.println("\n\t\t\t!==========CONVERSOR DE TEMPERATURAS SHOW DE BOLA==========!");
-            System.out.println("\n__________________________________________");
-            System.out.println("\n\t\t\\_____MENU____");
-            System.out.println("OPÇÕES DE CONVERSÃO DE TEMPERATURAS");
-            System.out.println("\n [1] - Conerter Celsius para Fahrenheit");
-            System.out.println("\n[2] - Converter Fahrenheit para Celsius");
-            System.out.println("\n[3] - Sair do Programa");
-
-            switch(conversor){
-
-                case 1:
-
-                    System.out.println("Por favor informe a temperatura em ºC: ");
-                    vCel = entrada.nextDouble();
-
-                    System.out.println("\n EXECUTANDO CONVERSÃO POR FAVOR AGUARDE");
-                    fahrenheit = (9 * vCel / 5) + 32;
-
-                    System.out.println("SUA CONVERSÃO ESTA PRONTA\n");
-                    System.out.printf("A TEMPERATURA EM FAHRENHEIT É %.2f\n", fahrenheit);
-                    break;
-
-                case 2:
-
-                    System.out.println("Por favor informe a temperatura em ºF: ");
-                     vFah = entrada.nextDouble();
-
-                    System.out.println("\n EXECUTANDO CONVERSÃO POR FAVOR AGUARDE");
-                    celsius = (vFah-32) * 5 / 9;
-
-                    System.out.printf("SUA CONVERSÃO ESTA PRONTA\n");
-                    System.out.println("\n A TEMPERATURA EM CELSIUS É %.2f\n");
-                    break;
-
-                case 3:
 
 
 
-
-
-            }
-
-        }while (conversor != 3 );
+    while (conversor != 3 );
 
 
     }
